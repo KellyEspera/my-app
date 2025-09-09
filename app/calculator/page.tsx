e24dc083920750829ef800d0faccd47b33e69096
James Kelly Espera <esperajameskelly123@gmail.com>
2025-09-09 17:26:11 +0800

    Update page.tsx

--- app/calculator/page.tsx
+++ app/calculator/page.tsx
@@ -3,8 +3,9 @@ import Index from "./calculator";
 export default function Calculator() { 
 
     return (
-        <div style={{ color: 'blue' }}>Calculator Page
-        
+        <div style={{ color: 'blue' }}>
+            
+            Calculator Page
         
         </div>
         
